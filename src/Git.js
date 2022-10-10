4e803da7ce6b8f0950222b18e6bfb2a88f918a0c
Shrutigupta431 <sg9956855@gmail.com>
2022-10-10 14:37:57 +0530

    third commit

---
+++ src/Git.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Git() {
+    return (
+      <div className="App">
+        <h1>
+        lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34lorem34
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Git;
+  
